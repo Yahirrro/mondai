c3b8c9b6caeb9388597c603f1b1581e05d86096e
Yahiro Hoshino <yahiro0921@gmail.com>
2020-12-30 17:09:12 +0900

    :sparkles: Add Next Font Optimizations

--- src/components/ui/layout/AppLayout.tsx
+++ src/components/ui/layout/AppLayout.tsx
@@ -27,6 +27,14 @@ export const AppLayout: React.FunctionComponent<Props> = (props) => {
     <>
       <Head>
         <link rel="icon" href="/logo/LogoIcon.png" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@400;700&display=swap"
+        />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter:wght@400;700&display=swap"
+        />
       </Head>
       <DefaultSeo
         dangerouslySetAllPagesToNoIndex={true}
